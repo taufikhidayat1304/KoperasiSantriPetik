e8997397641bf1b3584650ad66dec6eb0058938f
taufikhidayat1304 <taufikhidayt1304@.com>
2019-04-11 11:14:21 +0700

    commit susses

---
+++ app/src/main/java/com/example/koperasisantripetik/HomePage.java
@@ -0,0 +1,13 @@
+package com.example.koperasisantripetik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_homepagee);
+    }
+}
